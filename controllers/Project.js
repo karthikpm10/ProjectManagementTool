dd6e159b29bac3024d1653d4e8b8d7fca6c681d6
DESKTOP-H0BOTDT\Administrator <karthikpm10@gmail.com>
2018-04-16 02:00:02 -0400

    Created a Project  Controller, added the skeleton code to insert a Project

---
+++ controllers/Project.js
@@ -0,0 +1,19 @@
+var mongoose = require("mongoose");
+var Project = require("../models/Project");
+
+var projectController = {};
+
+
+// Insert a Project into Database
+projectController.insert = function (request, response) {
+    /*
+     create a Project Model object by using the parameters in the request object
+     call save on the Project Model object
+
+    */
+
+};
+
+
+
+module.exports = projectController;
