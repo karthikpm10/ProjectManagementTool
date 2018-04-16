945f926da63ea43f1445682e741c9e5ed34393ec
DESKTOP-H0BOTDT\Administrator <karthikpm10@gmail.com>
2018-04-16 15:08:43 -0400

    Added project insert functionality

--- controllers/Project.js
+++ controllers/Project.js
@@ -6,6 +6,21 @@ var projectController = {};
 
 // Insert a Project into Database
 projectController.insert = function (request, response) {
+    var projectObj = new Project();
+    projectObj.name = request.body.name;
+    projectObj.description = request.body.description;
+    projectObj.lead=request.body.lead;
+    projectObj.members=request.body.members.split(",");
+    projectObj.start_date=request.body.start_date;
+    projectObj.end_date=request.body.end_date;
+    projectObj.sprints=request.body.sprints;
+    projectObj.comments=request.body.comments;
+    projectObj.save(function (err, resp) {
+        if (err) return console.error(err);
+        
+      });
+
+   
     /*
      create a Project Model object by using the parameters in the request object
      call save on the Project Model object
