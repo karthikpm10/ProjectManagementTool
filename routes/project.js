1c247d753374e2e62b737a32d68b0c0d242c0007
LAPTOP-RSKTMJ37\Gautham <gauthi.kanekal@gmail.com>
2018-04-16 23:38:31 -0400

    List of Projects UI

--- routes/project.js
+++ routes/project.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/', function(req, res, next) {
+  //console.log(req.params.id);
+  res.render('project', { title: 'Project page' });
+});
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/:id', function(req, res, next) {
+  console.log(req.params.id);
   res.render('project', { title: 'Project page' });
 });
 
