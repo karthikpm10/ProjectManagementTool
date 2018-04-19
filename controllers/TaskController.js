9b309857aa97508248d1e32be41bb0f7e43b7790
pampas93 <abhijit.93@hotmail.com>
2018-04-19 02:00:52 -0400

    Edit sprints UI done - Need to test

--- controllers/TaskController.js
+++ controllers/TaskController.js
@@ -12,13 +12,15 @@ var taskController = {};
 //add task to a Sprint
 taskController.addTask = async (req, res) => {
 
+    //You'll be getting the name, description, assignee, start_date, end_date
+    //If assignee is null, leave it empty and make isAssigned to false
 
 };
 
 // list tasks of a sprint
 taskController.listTasks = async (req, res) => {
 
-
+//Need project, sprint and task details while render to front-end
 
 };
 
