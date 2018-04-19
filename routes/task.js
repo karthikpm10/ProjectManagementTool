fd955a3f51cb4086e27daff4edf8b96b4c75756d
pampas93 <abhijit.93@hotmail.com>
2018-04-19 16:59:37 -0400

    Update task

--- routes/task.js
+++ routes/task.js
@@ -4,6 +4,7 @@ var taskdetails = require("../controllers/TaskDetailsController");
 var Sprints = require("../models/Sprint");
 var Projects = require("../models/Project");
 var Tasks = require("../models/Task");
+var mongoose = require("mongoose");
 
 /* GET home page. */
 // router.get('/', function(req, res, next) {
@@ -13,15 +14,47 @@ var Tasks = require("../models/Task");
 router.get('/:id', async (req, res, next) => {
   //list tasks
   console.log(req.params.id);
-  var task = await Tasks.findOne({task_id: req.params.id});
-  var sprint = await Sprints.findOne({sprint_id: task.sprint_id})
-  var project = await Projects.findOne({project_id: sprint.project_id})
+  var task = await Tasks.findOne({ task_id: req.params.id });
+  var sprint = await Sprints.findOne({ sprint_id: task.sprint_id })
+  var project = await Projects.findOne({ project_id: sprint.project_id })
 
-  if(task == null)
-    res.render('index', { status: JSON.stringify(status), user: req.user});
+  if (task == null)
+    res.render('index', { status: JSON.stringify(status), user: req.user });
   else
     res.render('task', { title: 'Task page', user: req.user, project: project, sprint: sprint, task: task });
-  
+
+});
+
+router.post('/:id/EditTask', async (req, res, next) => {
+
+  Tasks.findOne({ task_id: new mongoose.Types.ObjectId(req.params.id) }, function (err, doc) {
+    if (err) {
+      console.log("update sprint failed");
+      res.redirect('/task/' + req.params.id);
+    }
+    else {
+      doc.name = req.body.name;
+      doc.description = req.body.description;
+      doc.start_date = req.body.startDate;
+      doc.end_date = req.body.endDate;
+      doc.status = req.body.status;
+      doc.assignee = req.body.assignee;
+      doc.save(function (err, resp) {
+        if (err) {
+          console.log("update task failed");
+          res.redirect('/task/' + req.params.id);
+        }
+        else {
+          console.log("updated sprint successfully");
+        }
+      }
+      );
+
+    }
+    res.redirect('/task/' + req.params.id);
+  });
+
+
 });
 
 
