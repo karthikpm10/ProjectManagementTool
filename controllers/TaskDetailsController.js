f2b79e7a5e3cbf84545441e708842e34950e7d29
pampas93 <abhijit.93@hotmail.com>
2018-04-19 16:34:02 -0400

    Task dashboard

---
+++ controllers/TaskDetailsController.js
@@ -0,0 +1,12 @@
+var mongoose = require("mongoose");
+var Project = require("../models/Project");
+var Users = require("../models/User");
+var Sprints = require("../models/Sprint");
+var Tasks = require("../models/Task");
+var JSAlert = require("js-alert");
+
+var taskDetailsController = {};
+
+
+
+module.exports = taskDetailsController;
--- routes/task.js
+++ routes/task.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var taskdetails = require("../controllers/TaskDetailsController");
+var Sprints = require("../models/Sprint");
+var Projects = require("../models/Project");
+var Tasks = require("../models/Task");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('task', { title: 'Task Page' });
+// router.get('/', function(req, res, next) {
+//   res.render('task', { title: 'Task Page' });
+// });
+
+router.get('/:id', async (req, res, next) => {
+  //list tasks
+  console.log(req.params.id);
+  var task = await Tasks.findOne({task_id: req.params.id});
+  var sprint = await Sprints.findOne({sprint_id: task.sprint_id})
+  var project = await Projects.findOne({project_id: sprint.project_id})
+
+  if(task == null)
+    res.render('index', { status: JSON.stringify(status), user: req.user});
+  else
+    res.render('task', { title: 'Task page', user: req.user, project: project, sprint: sprint, task: task });
+  
 });
 
 
